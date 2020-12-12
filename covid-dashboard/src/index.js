0c7dc44fc9f4f07dbac6ae3f3bf0d857bc21242f
Dmitry Dutin <dmitrydutin@gmail.com>
2020-12-12 23:14:06 +0300

    feat: add jest

--- covid-dashboard/src/index.js
+++ covid-dashboard/src/index.js
@@ -1,7 +1,7 @@
 import 'normalize.css';
 import './index.scss';
 
-import AppDOM from './plugins/app-dom';
+import AppDOM from './extensions/app-dom';
 import App from './app/app';
 
 window.addEventListener('DOMContentLoaded', () => {
