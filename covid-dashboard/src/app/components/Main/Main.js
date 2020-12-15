b8caa8984fca1db3542dee265d2774b3fd307074
Dmitry Dutin <dmitrydutin@gmail.com>
2020-12-15 21:34:42 +0300

    feat: create the observer pattern

--- covid-dashboard/src/app/components/Main/Main.js
+++ covid-dashboard/src/app/components/Main/Main.js
@@ -3,29 +3,32 @@ import CovidList from './CovidList/CovidList';
 import CovidMap from './CovidMap/CovidMap';
 import CovidTable from './CovidTable/CovidTable';
 import CovidDiagram from './CovidDiagram/CovidDiagram';
+import Store from '../Store/store';
 
 export default class Main {
-    #covidList = null;
-    #covidMap = null;
-    #covidTable = null;
-    #covidDiagram = null;
-
-    constructor() {
-        this.#covidList = new CovidList().render();
-        this.#covidMap = new CovidMap().render();
-        this.#covidTable = new CovidTable().render();
-        this.#covidDiagram = new CovidDiagram().render();
-    }
+    #main = null;
 
     render() {
-        const main = document.createElement('main');
-        main.classList.add('main');
+        this.#main = document.createElement('main');
+        this.#main.classList.add('main');
 
-        main.append(this.#covidList);
-        main.append(this.#covidMap);
-        main.append(this.#covidTable);
-        main.append(this.#covidDiagram);
+        this.#fill(null);
+        Store.subscribe(this.#fill.bind(this));
 
-        return main;
+        return this.#main;
+    }
+
+    #fill(country) {
+        this.#main.innerHTML = '';
+
+        const covidList = new CovidList(country).render();
+        const covidMap = new CovidMap(country).render();
+        const covidTable = new CovidTable(country).render();
+        const covidDiagram = new CovidDiagram(country).render();
+
+        this.#main.append(covidList);
+        this.#main.append(covidMap);
+        this.#main.append(covidTable);
+        this.#main.append(covidDiagram);
     }
 }
